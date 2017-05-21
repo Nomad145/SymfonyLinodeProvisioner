e4004a23c34c8384bb5f510bc11fef3febae595c
Michael Phillips <michaeljoelphillips@gmail.com>
2017-05-21 00:50:18 -0500

    Mock vendor API.

--- tests/AppBundle/Controller/LinodeControllerTest.php
+++ tests/AppBundle/Controller/LinodeControllerTest.php
@@ -5,24 +5,57 @@ namespace Tests\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Service\LinodeHostService;
 use AppBundle\Model\Linode;
+use Linode\AvailApi;
+use AppBundle\Factory\LinodeApiFactory;
+use Linode\LinodeApi;
+use Linode\Linode\IpApi;
+use AppBundle\Model\DataCenter;
+use AppBundle\Service\LinodeAvailService;
 
 class LinodeControllerTest extends WebTestCase
 {
     public function setUp()
     {
         $this->client = static::createClient();
-    }
 
-    public function testIndex()
-    {
-        $this->client = static::createClient();
-        $service = $this->createMock(LinodeHostService::class);
-        $service->method('getLinodes')
+        $availApi = $this->createMock(AvailApi::class);
+        $hostApi = $this->createMock(LinodeApi::class);
+        $ipApi = $this->createMock(IpApi::class);
+
+        $map = [
+            ['AvailApi', $availApi],
+            ['LinodeApi', $hostApi],
+            ['Linode\\IpApi', $ipApi],
+        ];
+
+        $factory = $this->createMock(LinodeApiFactory::class);
+        $factory
+            ->method('get')
+            ->will($this->returnValueMap($map));
+
+        $hostService = $this->createMock(LinodeHostService::class);
+        $hostService
+            ->expects($this->any())
+            ->method('getLinodes')
             ->willReturn([
                 (new Linode())->setLabel('Test Linode')
             ]);
 
-        $this->client->getContainer()->set('linode.api.host', $service);
+        $availService = $this->createMock(LinodeAvailService::class);
+        $availService
+            ->expects($this->any())
+            ->method('getDataCenters')
+            ->willReturn([
+                (new DataCenter())->setLocation('Test Location')
+            ]);
+
+        $this->client->getContainer()->set('linode.factory', $factory);
+        $this->client->getContainer()->set('linode.api.host', $hostService);
+        $this->client->getContainer()->set('linode.api.avail', $availService);
+    }
+
+    public function testIndex()
+    {
         $crawler = $this->client->request('GET', '/');
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
@@ -31,7 +64,6 @@ class LinodeControllerTest extends WebTestCase
 
     public function testCloneAction()
     {
-        $this->client = static::createClient();
         $crawler = $this->client->request('GET', '/clone');
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
@@ -39,7 +71,6 @@ class LinodeControllerTest extends WebTestCase
 
     public function testCreateAction()
     {
-        $this->client = static::createClient();
         $crawler = $this->client->request('GET', '/create');
 
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
